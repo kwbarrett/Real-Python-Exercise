ad71bf44b6f95f73a70ab976fa4120fd366ee5b2
Kenneth Barrett <kenneth.barrett.jr@gmail.com>
2020-03-02 23:52:20 -0500

    Last chapter 4 exerises

--- ch4/ex4_6.py
+++ ch4/ex4_6.py
@@ -1,13 +1,27 @@
-age = "49"
+# 1. Create a string containing an integer, then convert that string into
+# an actual integer object using int(). Test that your new object is
+# a number by multiplying it by another number and displaying the
+# result.
+word = "49"
+print(int(word)* 2)
 
-print("Ken is " + str(age) + " years old.")
+# 2. Repeat the previous exercise, but use a floating-point number and
+# float().
+print(float(word) * 2)
 
-new_age = int(age) * 2
+# 3. Create a string object and an integer object, then display them sideby-
+# side with a single print statement by using the str() function.
+my_string = "50"
+my_integer = 50
+print(my_string + " " + str(my_integer))
 
-print(new_age)
+# 4. Write a script that gets two numbers from the user using the
+# input() function twice, multiplies the numbers together, and
+# displays the result. If the user enters 2 and 4, your program should
+# print the following text:
+# The product of 2 and 4 is 8.0.
 
-age2 = "49.0"
-print("Ken is " + str(age2) + " years old.")
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
 
-new_age2 = float(age2)
-print(new_age2)
+print(f"The product of {num1} and {num2} is {float(num1) * float(num2)}")
