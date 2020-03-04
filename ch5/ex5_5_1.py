2f7e7f334600fb3a37ecd0fc4c17702dfd86165b
Kenneth Barrett <kenneth.barrett@mncppc.org>
2020-03-04 12:02:38 -0500

    Chapter 5 exercises take 2

--- ch5/ex5_5_1.py
+++ ch5/ex5_5_1.py
@@ -1,3 +1,4 @@
 myNum = input("Enter a number: ")
 result = float(myNum)
-print("{} rounded to 2 decimal places is {}".format(myNum, round(result,2)))
+# print("{} rounded to 2 decimal places is {}".format(myNum, round(result,2)))
+print(f"{myNum} rounded to 2 decimal places is {round(result, 2)}.")
--- ch5/ex5_6.py
+++ ch5/ex5_6.py
@@ -1,7 +1,7 @@
 # Print the result of the calculation 3 ** .125 as a fixed-point number 
 # with three decimal places.
 
-print(f"{3 ** .125:.3}")
+print(f"{3 ** .125:.3f}")
 
 print(f"${150000:,.2f}")
 
